02e049a9004df03e8e5038c6da118d68ea2d62a6
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2023-09-12 16:57:25 +0530

    saving selected data in LS

--- react--module/src/Components/ListPart/List.js
+++ react--module/src/Components/ListPart/List.js
@@ -1,22 +1,49 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import '../PopUpBox/PopUp.css';
 import './Styles/ListPart.css';
 const List = () => {
   const [create, setCreate] = useState(false);
-
   const Myref = useRef(null);
+  const NameRef = useRef(null);
+
+  const [selectedData, setSelectedata] = useState({ name: '', color: '' });
 
   function CLKCreateButton(event) {
     setCreate(!create);
   }
 
-  window.addEventListener('click', Ckl);
+  useEffect(() => {
+    window.addEventListener('click', Ckl);
+  }, []);
 
   function Ckl(e) {
     if (e.target.className === 'main--container') {
       console.log('Clicked outSide');
       setCreate(false);
     }
+  }
+
+  let DataArray = JSON.parse(localStorage.getItem('AllData'));
+  if (!DataArray) {
+    DataArray = [];
+  }
+  //Name Enter
+
+  function SelectColor(e) {
+    const SelectedColor = e.target.className;
+    setSelectedata((pre) => {
+      return { ...pre, color: SelectedColor };
+    });
+  }
+
+  //When we click create button
+  function NameFun(e) {
+    let TyedName = NameRef.current.value;
+    console.log(TyedName);
+
+    DataArray.push({ name: TyedName, color: selectedData.color });
+    localStorage.setItem('AllData', JSON.stringify(DataArray));
+    console.log(DataArray);
   }
 
   return (
@@ -34,25 +61,36 @@ const List = () => {
               <p>Group name</p>
               <input
                 type="text"
-                name=""
-                id=""
                 placeholder="Enter your group name...."
+                value={selectedData.name}
+                onChange={(e) => {
+                  setSelectedata((prev) => {
+                    return { ...prev, name: e.target.value };
+                  });
+                }}
+                ref={NameRef}
               />
             </div>
             <br />
             <div className="Choose-color--container">
               <p>Choose color</p>
-              <ul className="color-li">
-                <li id="Purple"></li>
-                <li id="pink"></li>
-                <li id="sky"></li>
-                <li id="brown"></li>
-                <li id="blue"></li>
-                <li id="light--blue"></li>
+              <ul className="color-li" value={selectedData.color}>
+                <li id="Purple" className="#b38bfa" onClick={SelectColor}></li>
+                <li id="pink" className="#ff79f2" onClick={SelectColor}></li>
+                <li id="sky" className="#43e6fc" onClick={SelectColor}></li>
+                <li id="brown" className="#f19576" onClick={SelectColor}></li>
+                <li id="blue" className="#0047ff" onClick={SelectColor}></li>
+                <li
+                  id="light--blue"
+                  className="#6691ff"
+                  onClick={SelectColor}
+                ></li>
               </ul>
             </div>
             <br />
-            <button className="Create--button">Create</button>
+            <button className="Create--button" onClick={NameFun}>
+              Create
+            </button>
           </div>
         </div>
       ) : (
